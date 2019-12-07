bdb819251148ff1074b4f6b5583bd54eeb74015d
devopsapple <58291752+devopsapple@users.noreply.github.com>
2019-12-07 13:41:01 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -5,4 +5,4 @@ export KUBECONFIG=
 
 export NAMESPACE=tektondevops-18
 export EMAIL=devopsapple@uscwifi.cn
-export INGRESS=
+export INGRESS=tektonknativebeijing18-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
