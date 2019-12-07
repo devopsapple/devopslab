dd6bf0785b67547b6d4254651851f04c4770b318
devopsapple <58291752+devopsapple@users.noreply.github.com>
2019-12-07 13:30:28 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -3,6 +3,6 @@ export GITACCOUNT=devopsapple
 export MYCLUSTER=s0KwYoCg7y3-8ojkzMYjIDAzvYO90NQNj9tQh_xbxZWC
 export KUBECONFIG=
 
-export NAMESPACE=
+export NAMESPACE=tektondevops-18
 export EMAIL=devopsapple@uscwifi.cn
 export INGRESS=
